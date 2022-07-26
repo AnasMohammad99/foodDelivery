d49d18cb81d77d568d03d13ce4577e38e0f585eb
ans mohammad <ammamasz.basha@gmail.com>
2022-07-26 22:48:25 +0200

    changes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -16,8 +16,8 @@ export class AuthController {
   signup(@Body() dto: AuthDto) {
     return this.authService.signup(dto);
   }
-
   @HttpCode(HttpStatus.OK)
+  
   @Post('signin')
   signin(@Body() dto: AuthDto) {
     return this.authService.signin(dto);
